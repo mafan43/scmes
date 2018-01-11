c663b0fd3eebad79b412d1f8d7878275429ef966
mafan43 <mafan43@gmail.com>
2018-01-11 12:50:09 +0700

    Update Home.php

--- Home.php
+++ Home.php
@@ -75,6 +75,8 @@
                 text-decoration: none;
             }
       </style>
+   <script type="text/javascript">function add_chatinline(){var hccid=12092283;var nt=document.createElement("script");nt.async=true;nt.src="https://mylivechat.com/chatinline.aspx?hccid="+hccid;var ct=document.getElementsByTagName("script")[0];ct.parentNode.insertBefore(nt,ct);}
+add_chatinline();</script>
   </head>
    <body>
    <nav>
